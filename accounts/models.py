dab2228749bb91cab026faaf8ea3266377db337d
Yusuf <yusufbjk__44@hotmail.com>
2024-05-08 14:52:52 +0300

    models

--- accounts/models.py
+++ accounts/models.py
@@ -8,6 +8,12 @@ class CustomerProfile(models.Model):
 
     def __str__(self):
         return self.user.username + "'s Profile"
+    
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 class RestaurantProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -15,16 +21,11 @@ class RestaurantProfile(models.Model):
     address = models.CharField(max_length=255)
     image = models.ImageField(upload_to='restaurants/')
     minimum_order_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ManyToManyField(ProductCategory,related_name="categories_in_restaurants" )
 
     def __str__(self):
         return self.name + "'s Profile"
 
-
-class ProductCategory(models.Model):
-    name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
@@ -40,6 +41,7 @@ class Product(models.Model):
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     products = models.ManyToManyField(Product, through='OrderItem')
+    restaurant = models.ForeignKey(RestaurantProfile, on_delete=models.CASCADE, null=True)
     order_date = models.DateTimeField(auto_now_add=True)
     is_cancelled = models.BooleanField(default=False)
 
